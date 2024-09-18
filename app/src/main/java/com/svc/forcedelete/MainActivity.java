fdd612ba03dbab5bfae02aa9a0a034c9d276e585
Oktay Savcı <93824768+oktaysavci@users.noreply.github.com>
2024-09-18 14:22:20 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/svc/forcedelete/MainActivity.java
@@ -0,0 +1,15 @@
+package com.svc.forcedelete;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.*;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startService(new Intent(this, MyService.class));
+        finish(); // MainActivity'yi kapat
+    }
+}
+
